2296b52ab4328a44018d5fcecc18ae116526b33f
Oswaldo Castro <tufcoder@outlook.com>
2022-12-22 12:10:35 -0300

    refactoring: add constants and small changes

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
 const MAX_ATTRIBUTES = 6;
-const MINIMAL_SCORE = 12;
+const MINIMAL_SCORE = 13;
+
+console.log(getScores(MAX_ATTRIBUTES));
 
 function getScores(totalOfAttributes) {
   const scores = new Array(totalOfAttributes);
 
   for (let i = 0; i < totalOfAttributes; i++) {
-    scores[i] = (setMinimalScore(roll4D6()));
+    scores[i] = getRule();
   }
 
-  return scores;
+  return scores.sort((a, b) => b - a);
 }
 
-function setMinimalScore(score) {
+function getRule() {
+  return (setMinimalScore(ruleRoll4D6));
+}
+
+function setMinimalScore(rule) {
+  let score = rule();
+
   while (score < MINIMAL_SCORE) {
-    score = roll4D6();
+    score = rule();
   }
 
   return score;
 }
 
-function roll4D6() {
+function ruleRoll4D6() {
   const values = rollDice(4, 6);
   values.sort((a, b) => b - a);
   values.pop();
 
-  let total = values.reduce((total, value) => total += value);
+  const total = values.reduce((accumulator, value) =>
+    accumulator += value, 0);
 
   return total;
 }
 
-function rollDice(number, dice) {
-  const numbers = new Array(number);
+function rule10Plus1D8() {
+  const values = rollDice(1, 8);
 
-  for (let i = 0; i < number; i++) {
-    numbers[i] = getRandomNumber(dice);
+  return 10 + values[0];
+}
+
+function rule8Plus1D10() {
+  const values = rollDice(1, 10);
+
+  return 8 + values[0];
+}
+
+function rollDice(total, dice) {
+  const numbers = new Array(total);
+
+  for (let i = 0; i < total; i++) {
+    numbers[i] = getRandomNumber(1, dice);
   }
 
   return numbers;
 }
 
-function getRandomNumber(number) {
-  return Math.floor(Math.random() * number) + 1;
+function getRandomNumber(min, total) {
+  return Math.floor(Math.random() * total) + min;
 }
-
-console.log(getScores(MAX_ATTRIBUTES));
